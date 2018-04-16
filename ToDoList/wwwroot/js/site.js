bd7ad629ef172fed628b6b32650272740bc300ea
Student <example@example.com>
2018-04-16 16:15:31 -0700

    update methods

--- ToDoList/wwwroot/js/site.js
+++ ToDoList/wwwroot/js/site.js
@@ -1,1 +1,19 @@
 ﻿// Write your Javascript code.
+
+$("input[type=checkbox]").change(function() {
+  if (this.checked) {
+    $(".strike").css('text-decoration', 'line-through');
+  } 
+ 
+});
+
+function taskDone(id,  checkBoxId) {
+    var c = document.getElementById(id);
+    var  checkBox = document.getElementById(checkBoxId); //getting checkbox
+    if (checkBox.checked) { //validating checked or not
+        c.className = "strike";
+    }
+    else {
+        c.className = "nostrike";
+    }
+}
