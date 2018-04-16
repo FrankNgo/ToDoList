e8f9ad4080c499fdb8e034a5372b9bfcfad989f7
Student <example@example.com>
2018-04-16 12:00:34 -0700

    add CRUD for categories

--- ToDoList/Controllers/CategoriesController.cs
+++ ToDoList/Controllers/CategoriesController.cs
@@ -3,17 +3,72 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace ToDoList.Controllers
 {
-    public class CategoriesController : Controller
+    public class CategoryController : Controller
     {
-        // GET: /<controller>/
+        private ToDoListContext db = new ToDoListContext();
         public IActionResult Index()
         {
+            //List<Item> model = db.Items.ToList();
+            //return View(model);
+            return View(db.Categories.Include(items => items.Name).ToList());
+        }
+
+        public IActionResult Details(int id)
+        {
+            //Item thisItem = db.Items.FirstOrDefault(items => items.ItemId == id);
+            var thisCatagory = db.Categories.FirstOrDefault(items => items.CategoryId == id);
+            return View(thisCatagory);
+        }
+
+        public IActionResult Create()
+        {
+            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "Name");
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            db.Categories.Add(category);
+            db.SaveChanges();
+            return RedirectToAction("Index");//Maybe want to change to difference route
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var thisCategory = db.Categories.FirstOrDefault(items => items.CategoryId == id);
+            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "Name");
+            return View(thisCategory);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            db.Entry(category).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var thisCategory = db.Categories.FirstOrDefault(items => items.CategoryId == id);
+            return View(thisCategory);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var thisCategory = db.Categories.FirstOrDefault(items => items.CategoryId == id);
+            db.Categories.Remove(thisCategory);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
-}
+}   
